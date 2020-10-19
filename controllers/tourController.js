73dacb17f73768086b726935ba225c0ea8e4f2b3
Ahmed Reda <manoa15@yahoo.com>
2020-10-19 15:23:43 +0300

    finishing findingAllTours and findingTourById controllers

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,14 +1,28 @@
 const Tour = require('./../models/tourModel')
 
-exports.getAllTours = (req, res) => {
-    res.json({
-        status: 'success'
-       
-    })
+
+exports.getAllTours = async (req, res) => {
+
+    try{
+            tours = await Tour.find()
+                res.json({
+                    status: 'success',
+                    numberOfResults : tours.length,
+                    result: tours
+                
+                })
+
+    }catch(err){
+                res.status(400).json({
+                    status:'fail',
+                    message:'invalid data'
+                })
+            }
 
 }
 
 exports.addNewTour = async (req, res) => {
+    
     try{
     newTour = await Tour.create(req.body)
 
@@ -16,25 +30,29 @@ exports.addNewTour = async (req, res) => {
         status: 'success',
         data: newTour
         })
-    }
-    catch(err){
+    }catch(err){
         res.status(400).json({
             status:'fail',
             message:'invalid data'
         })
         
     }
-
 }
 
 
-exports.getTourById =  (req, res) => {
-    
-   
-  
-    res.json({
-        status: 'success'
-        
-    })    
-  }
+exports.getTourById =  async (req, res) => {
+
+    try{
+            foundTour = await Tour.findById(req.params.id)
+            res.status(200).json({
+                status: 'success',
+                data: foundTour
+                })
+            
+    }catch(err){
+        res.status(400).json({
+            status:'fail',
+            message:'invalid data'
+        })
+    }}
 
